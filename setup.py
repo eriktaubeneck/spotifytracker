24de19df0019cf4ce19c6deeea4cc0fdf9187d6a
Erik Taubeneck <erik.taubeneck@gmail.com>
2016-09-20 13:48:04 -0400

    add missing stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='spotifytracker',
-    version='0.0.12',
+    version='0.0.13',
     packages=['spotify_tracker'],
     url='http://github.com/eriktaubeneck/spotifytracker',
     license='MIT',
